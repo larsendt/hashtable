0dabd942264c0f5547267bedffd3b566a27c9792
Dane Larsen <dane.t.larsen@gmail.com>
2012-07-13 14:00:20 -0600

    initial framework of md5, debug flag in makefile

--- src/MD5Digest.cpp
+++ src/MD5Digest.cpp
@@ -8,9 +8,41 @@
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
+#define F(X, Y, Z) (X & Y) | ((~X) & Z)
+#define G(X, Y, Z) (X & Z) | (Y & (~Z))
+#define H(X, Y, Z) X ^ Y ^ Z
+#define I(X, Y, Z) Y ^ (X | (~Z))
+
+
+uint get_fullsize(uint64_t size);
+void print_fulldata(uchar *fulldata, uint fullsize);
+
 MD5Digest::MD5Digest(const uchar *data, uint64_t size)
 {
+    debug("Size: %d", size);
+
+    int a = 0x67452301;
+    int b = 0xefcdab89;
+    int c = 0x98badcfe;
+    int d = 0x10325476;
+
+    uint fullsize = get_fullsize(size);
+    uchar *fulldata = new uchar[fullsize];
+
+    print_fulldata(fulldata, fullsize);
+
+    delete[] fulldata;
 }
 
 
+uint get_fullsize(uint64_t size)
+{
+    uint n = (size + 9) / 64; 
+    uint fullsize = n * 64;
+    return fullsize;
+}
 
+void print_fulldata(uchar *fulldata, uint fullsize)
+{
+    debug("Fulldata:");
+}
