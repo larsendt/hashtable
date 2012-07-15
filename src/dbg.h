df44322aba4c9b76ea8bfec2cf4bf689b0daf2c4
Dane Larsen <dane.t.larsen@gmail.com>
2012-07-15 14:11:28 -0600

    added color code defines

--- src/dbg.h
+++ src/dbg.h
@@ -5,22 +5,22 @@
 #include <errno.h>
 #include <string.h>
 
-// RED \033[22;31m
-// GREEN \033[22;32m
-// BROWN \033[22;33m
-// BLUE \033[22;34m
-// MAGENTA \033[22;35m
-// CYAN \033[22;36m
-// GRAY \033[22;37m
-// DGRAY \033[01;30m
-// LRED \033[01;31m
-// LGREEN \033[01;32m
-// YELLOW \033[00;33m
-// LBLUE \033[01;34m
-// LMAGENTA \033[01;35m
-// LCYAN \033[01;36m
-// WHITE \033[01;37m
-// NORM \033[0m
+#define RED(M) "\033[22;31m"M"\033[0m"
+#define GREEN(M) "\033[22;32m"M"\033[0m"
+#define BROWN(M) "\033[22;33m"M"\033[0m"
+#define BLUE(M) "\033[22;34m"M"\033[0m"
+#define MAGENTA(M) "\033[22;35m"M"\033[0m"
+#define CYAN(M) "\033[22;36m"M"\033[0m"
+#define GRAY(M) "\033[22;37m"M"\033[0m"
+#define DGRAY(M) "\033[01;30m"M"\033[0m"
+#define LRED(M) "\033[01;31m"M"\033[0m"
+#define LGREEN(M) "\033[01;32m"M"\033[0m"
+#define YELLOW(M) "\033[00;33m"M"\033[0m"
+#define LBLUE(M) "\033[01;34m"M"\033[0m"
+#define LMAGENTA(M) "\033[01;35m"M"\033[0m"
+#define LCYAN(M) "\033[01;36m"M"\033[0m"
+#define WHITE(M) "\033[01;37m"M"\033[0m"
+#define NORM(M) "\033[0m"M"\033[0m"
 
 #ifdef VERBOSE_DEBUG
 
