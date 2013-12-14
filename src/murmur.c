2cfff7b8c9d3f2dda4144c8e46e2dd67e78fc5df
Matthias Beyer <mail@beyermatthias.de>
2013-12-14 17:13:24 +0100

    removed unnecessary whitespace
    This patch removes some unnecessary whitespace.

--- src/murmur.c
+++ src/murmur.c
@@ -59,8 +59,7 @@ FORCE_INLINE uint64_t fmix64(uint64_t k)
 
 //-----------------------------------------------------------------------------
 
-void MurmurHash3_x86_32 ( const void * key, int len,
-        uint32_t seed, void * out )
+void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out )
 {
     const uint8_t * data = (const uint8_t*)key;
     const int nblocks = len / 4;
@@ -115,8 +114,8 @@ void MurmurHash3_x86_32 ( const void * key, int len,
 
 //-----------------------------------------------------------------------------
 
-void MurmurHash3_x86_128 ( const void * key, const int len,
-        uint32_t seed, void * out )
+void MurmurHash3_x86_128(const void *key, const int len, uint32_t seed,
+        void *out)
 {
     const uint8_t * data = (const uint8_t*)key;
     const int nblocks = len / 16;
@@ -220,8 +219,8 @@ void MurmurHash3_x86_128 ( const void * key, const int len,
 
 //-----------------------------------------------------------------------------
 
-void MurmurHash3_x64_128 ( const void * key, const int len,
-        const uint32_t seed, void * out )
+void MurmurHash3_x64_128(const void *key, const int len, const uint32_t seed,
+       void *out)
 {
     const uint8_t * data = (const uint8_t*)key;
     const int nblocks = len / 16;
